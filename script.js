4408290c0aab364d305742c953d3f3e1e5086c0e
rhyo-arga <123200059@student.upnyk.ac.id>
2023-04-14 01:36:10 +0700

    Add Some Logic on script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function calculateBMI() {
 
     if (bmi < 18.5) {
         category = "Underweight";
-    } else if (bmi <= 24.9) {
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
         category = "Normal";
-    } else if (bmi <= 29.9) {
+    } else if (bmi >= 25 && bmi <= 29.9) {
         category = "Overweight";
     } else {
         category = "Obesity";
